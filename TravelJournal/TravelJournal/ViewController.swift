cf183a30cc26a91bd3802ded405a45a766085ff4
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-11-06 17:23:47 -0500

    Creating project

---
+++ TravelJournal/TravelJournal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelJournal
+//
+//  Created by Araceli Teixeira on 06/11/17.
+//  Copyright © 2017 Araceli Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
